7af82647a33860d3c36fbd0b932944a2a0d8e9a7
Nelson Pang <nelson00@connect.hku.hk>
2022-05-23 15:18:53 +0800

    update

--- scripts/startServer.sh
+++ scripts/startServer.sh
@@ -3,12 +3,13 @@ jarName="server.jar"
 cluster="trading"
 serverIPs="127.0.0.1:8081,127.0.0.1:8082,127.0.0.1:8083"
 
-#java -jar $dir/$jar_name "trading" "127.0.0.1:8081,127.0.0.1:8082,127.0.0.1:8083" "NonStopCreateAccount" "500"
-#/tmp/server1 trading 127.0.0.1:8081 127.0.0.1:8081,127.0.0.1:8082,127.0.0.1:8083
+logDir="log_server"
+mkdir -p $logDir
+
 for ((i=1; i<=3; i+=1)); do
     outputFile="tmp/server$i"
     IP="127.0.0.1:808$i"
-    logFile="log_server/log$i"
+    logFile="$logDir/log$i"
     echo "Starting Server $i. IP = $IP"
     java -jar $dir/$jarName $outputFile $cluster $IP $serverIPs > $logFile 2>&1 &
 done
